1f2eaa421865643f601190506aac584fb0bb38e1
Abdullah Ajmal <work.abdullahajmal@gmail.com>
2025-02-17 12:34:37 +0500

    added book storing functionality

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,54 @@
 import express, { response } from "express"
 import {PORT, DB} from "./config.js"
 import mongoose from "mongoose"
+import { Book } from "./models/bookModel.js"
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (request, response)=>{
-    // console.log(request)
     return response.status(200).send('[APP] working')
 })
 
-app.post('/books', async (request, res) => {
+app.post('/books', async (request, response) => {
     try {
+
+        if (!request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({msg: "Send all required fields"})
+        }
+
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        }
+
+        const book = await Book.create(newBook)
+        return response.status(201).send(book)
         
     } catch (error) {
         console.log(error)
         return response.status(500)
     }
 })
+
+app.get('/books', async (request, response)=> {
+    try {
+        const books = await Book.find({})
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({msg: error.message})
+    }
+})
+
+
 
 
 
